6c1f878bd38501bb6688a72e28a901ff0364521f
cyurtbil <cenky92@gmail.com>
2014-10-19 19:51:46 -0400

    Added attr_accessor to the user modeil for login purposes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
   has_many :wallets
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
