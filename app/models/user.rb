00d8de8a58c6b6fcebc8935ef2bb43a8556b3396
cyurtbil <cenky92@gmail.com>
2014-10-19 19:57:44 -0400

    Overwrite devise find_for_database_authentication method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,15 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :validatable, authentication_keys: [:login]
          :omniauthable, omniauth_providers: [:soundcloud]
+         
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+   end
 
    def self.from_omniauth(auth)
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
