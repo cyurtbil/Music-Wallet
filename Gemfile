8c5f0f3ba5d7c16771bab1332faa29d817fc3c02
cyurtbil <cenky92@gmail.com>
2014-10-19 19:47:12 -0400

    Added username to the users, refactored gemfile, models and controllers

--- Gemfile
+++ Gemfile
@@ -30,10 +30,9 @@ gem 'rails_12factor', group: :production
 # Use Unicorn as the app server
 # gem 'unicorn'
 gem 'devise'
-gem 'dotenv-rails'
 gem 'soundcloud'
 gem 'omniauth'
-gem 'omniauth-soundcloud'
+gem 'omniauth-soundcloud', '~> 1.0.0'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
@@ -47,5 +46,6 @@ group :development, :test do
   gem 'rspec-rails', '~> 3.0.0'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'dotenv-rails'
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,18 @@
 class User < ActiveRecord::Base
-	has_many :wallets
+  has_many :wallets
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:soundcloud]
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-    end
+   def self.from_omniauth(auth)
+     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+       user.provider = auth.provider
+       user.uid = auth.uid
+       # user.email = auth.info.email
+       user.password = Devise.friendly_token[0,20]
+     end
   end
 
   def self.new_with_session(params, session)
@@ -22,5 +22,5 @@ class User < ActiveRecord::Base
       end
     end
   end
-  
+
 end
