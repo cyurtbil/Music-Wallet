0a25586364cca333e0a44928fe80a868c3e14d59
cyurtbil <cenky92@gmail.com>
2014-10-20 12:09:49 -0400

    Now it can display 10 hottest tracks chosen by soundcloud

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
 	def show
+		client = Soundcloud.new(client_id: ENV['SOUNDCLOUD_CLIENT_ID'],
+                        client_secret: ENV['SOUNDCLOUD_SECRET'])
+		@tracks = client.get('/tracks', :limit => 10, :order => 'hotness')
 	end
 end
