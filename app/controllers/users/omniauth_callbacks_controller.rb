2b78fbb73977fc6371977fff471d4cf093fc2efd
cyurtbil <cenky92@gmail.com>
2014-10-23 01:01:22 -0400

    Fixed the bug on signing in

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,14 +4,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def soundcloud
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    # binding.pry
-    # if @user.persisted?
+    binding.pry
+    unless @user.nil?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "Soundcloud") if is_navigational_format?
-    # else
-    #   session["devise.soundcloud_data"] = request.env["omniauth.auth"]
-    #   redirect_to new_user_registration_url
-    # end
+    else
+      session["devise.soundcloud_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
   end
   # You should also create an action method in this controller like this:
   # def twitter
