8957f9f68ee935eaa3ec95808eee92c2fe3726e7
cyurtbil <cenky92@gmail.com>
2014-11-03 20:36:56 -0500

    Added new gem for filtering, fixed a bug in wallets create method

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -2,7 +2,7 @@ class Wallet < ActiveRecord::Base
 	belongs_to :user
 	has_many :songs
 
-	validates :name, presence: true, uniqueness: true , on: :create
+	validates :name, presence: true, on: :create
 
   # scope :filtered, -> { where(checked: true) }
 end
