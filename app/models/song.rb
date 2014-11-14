c3fcefbfa0d84dfe9526cf83de8b4d852f0180cf
cyurtbil <cenky92@gmail.com>
2014-11-14 09:48:21 -0500

    removed the wallet id validation on song model which can cause problems in heroku deployment

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,5 @@ class Song < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :wallet
 
-	validates :wallet_id, presence: true, on: :create
+	# validates :wallet_id, presence: true, on: :create
 end
